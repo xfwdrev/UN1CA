f2dbbd129277c9e36e8c23a9054c5e9bc605565f
Fred <frignacio16@gmail.com>
2026-04-09 23:59:46 +0800

    target: y2s: init
    Signed-off-by: Fred <frignacio16@gmail.com>

---
+++ target/y2s/debloat.sh
@@ -0,0 +1,12 @@
+# Copyright (c) 2025 Salvo Giangreco
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# Debloat list for Galaxy S20+ (Exynos) (y2s)
+# - Add entries inside the specific partition containing that file (<PARTITION>_DEBLOAT+="")
+# - DO NOT add the partition name at the start of any entry (eg. "/system/dpolicy_system")
+# - DO NOT add a slash at the start of any entry (eg. "/dpolicy_system")
+
+# Overlays
+SYSTEM_DEBLOAT+="
+system/app/WifiRROverlayAppH2E
+"
